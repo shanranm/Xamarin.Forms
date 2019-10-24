f9e447986201a9a549afa78e3994d248001bd087
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-10-24 12:45:44 +0200

    Added Switch CustomRenderer to Issue7249

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7249.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue7249.cs
@@ -46,11 +46,64 @@ namespace Xamarin.Forms.Controls.Issues
 				IsToggled = true
 			};
 
+			var instructions2 = new Label
+			{
+				Text = "The Switch below uses a Custom Renderer to validate that can override colors using a Custom Renderer."
+			};
+
+			var customSwitch = new Issue7249Switch
+			{
+				SwitchOffColor = Color.Red,
+			    SwitchOnColor = Color.Green,
+				SwitchThumbColor = Color.Yellow,
+				HorizontalOptions = LayoutOptions.Start,
+				IsToggled = false
+			};
+
 			layout.Children.Add(instructions);
 			layout.Children.Add(switch1);
 			layout.Children.Add(switch2);
+			layout.Children.Add(instructions2);
+			layout.Children.Add(customSwitch);
 
 			Content = layout;
+		}
+	}
+
+	[Preserve(AllMembers = true)]
+	public class Issue7249Switch : Switch
+	{
+		public static readonly BindableProperty SwitchOffColorProperty =
+		  BindableProperty.Create(nameof(SwitchOffColor),
+			  typeof(Color), typeof(CustomSwitch),
+			  Color.Default);
+
+		public Color SwitchOffColor
+		{
+			get { return (Color)GetValue(SwitchOffColorProperty); }
+			set { SetValue(SwitchOffColorProperty, value); }
+		}
+
+		public static readonly BindableProperty SwitchOnColorProperty =
+		  BindableProperty.Create(nameof(SwitchOnColor),
+			  typeof(Color), typeof(CustomSwitch),
+			  Color.Default);
+
+		public Color SwitchOnColor
+		{
+			get { return (Color)GetValue(SwitchOnColorProperty); }
+			set { SetValue(SwitchOnColorProperty, value); }
+		}
+
+		public static readonly BindableProperty SwitchThumbColorProperty =
+		  BindableProperty.Create(nameof(SwitchThumbColor),
+			  typeof(Color), typeof(CustomSwitch),
+			  Color.Default);
+
+		public Color SwitchThumbColor
+		{
+			get { return (Color)GetValue(SwitchThumbColorProperty); }
+			set { SetValue(SwitchThumbColorProperty, value); }
 		}
 	}
 }
